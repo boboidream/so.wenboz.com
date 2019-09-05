a50c5160bbf1905f097444b2fee5cd89aa84cc04
boboidream <z644381492@gmail.com>
2019-09-05 22:03:24 +0800

    [+] PWA Mode

---
+++ js/main.js
@@ -0,0 +1,344 @@
+// 点击搜索按钮
+function search() {
+  var e = document.getElementById("search_input");
+  var to = e.dataset.to
+
+  if (e.value != "") {
+    window.location.href = to + e.value
+  }
+  return false;
+}
+
+// 点击第一排搜索引擎
+var $box = $('.search_type .box')
+$box.on('click', function() {
+  var $this = $(this)
+  var val = $('#search_input').val()
+  var prefix = $this.data('to')
+  var logo = $this.data('logo')
+
+  if (logo) {
+    $('.logo img').removeClass('active')
+    $('#' + logo).addClass('active')
+    $('#search_input').attr('data-to', prefix)
+
+    var current = {
+      logo: logo,
+      prefix: prefix
+    }
+    localStorage.setItem('search', JSON.stringify(current))
+  }
+
+  window.location.href = prefix + val
+})
+
+// 点击单次搜索 LOGO
+var $once = $('.box.once')
+$once.on('click', function() {
+  var $this = $(this)
+  var val = $('#search_input').val()
+  var prefix = $this.data('to')
+  var url;
+
+  if (val === '') {
+    url = $this.data('home') ?
+      $this.data('home') : prefix.match(/https:\/\/.+?\//)[0]
+  } else {
+    url = prefix.replace('$$', val)
+  }
+  window.location.href = url
+})
+
+// 读取默认搜索
+$(function() {
+  var current = localStorage.getItem('search')
+
+  if (current) {
+    current = JSON.parse(current)
+    var prefix = current["prefix"]
+    var logo = current["logo"]
+
+    $('.logo img').removeClass('active')
+    $('#' + logo).addClass('active')
+    $('#search_input').attr('data-to', prefix)
+  }
+})
+
+// 隐秘模式==========================================
+var proxy_url = ['https://g.wenboz.com:8443/-----', 'https://zjcqoo.github.io/-----', 'https://gamedun.github.io/-----']
+
+// 网址可用性测试实例
+var net = new Net()
+
+// 🐵状态实例
+var monkey = new Monkey()
+
+// 锁定线路实例
+var lockNet = new LockNet(proxy_url)
+
+// 初始化
+$(function() {
+  var su = localStorage.getItem('SU')
+
+  monkey.init()
+
+  if (su === 'true') {
+    openSU()
+  } else {
+    nosu_test()
+  }
+
+  document.addEventListener("visibilitychange", handleVisibilityChange, false);
+})
+
+
+// Class 区================================
+function Net() {
+  var timelimit = 1500
+  this.p = new Ping({
+    timeout: timelimit
+  })
+  this.test = function(url) {
+    var p = this.p
+    var pro = new Promise(function(resolve, reject) {
+      p.ping(url, function(err, ping) {
+        console.log('时间：' + ping)
+        if (ping < timelimit) {
+          resolve(url)
+        } else {
+          reject('Timeout:' + url)
+        }
+      })
+    })
+
+    return pro
+  }
+}
+
+function Monkey() {
+  this.$dom = $('#monkey')
+  this.open = function() {
+    this.$dom.removeClass('hide').addClass('show')
+    localStorage.setItem('mk', 1)
+  }
+  this.close = function() {
+    this.$dom.removeClass('show').addClass('hide')
+    localStorage.setItem('mk', 0)
+  }
+  this.color = function() {
+    this.$dom.removeClass('gray').addClass('color')
+  }
+  this.gray = function() {
+    this.$dom.removeClass('color').addClass('gray')
+  }
+  this.init = function() {
+    var mk = localStorage.getItem('mk')
+
+    if (parseInt(mk)) {
+      this.open()
+    } else {
+      this.close()
+    }
+  }
+}
+
+function LockNet(arr) {
+  this.next = function(cb) {
+    var num = this.getNum()
+
+    if (!num) {
+      localStorage.setItem('lock', 1)
+      if (cb) cb(1, arr[0])
+      return arr[0]
+    }
+    if (num >= arr.length) {
+      localStorage.setItem('lock', 0)
+      if (cb) cb(0, false)
+      return false
+    }
+
+    localStorage.setItem('lock', ++num)
+
+    if (cb) cb(num, arr[num - 1])
+    return arr[num]
+  }
+  this.getNum = function() {
+    return parseInt(localStorage.getItem('lock'))
+  }
+  this.getVal = function() {
+    var num = this.getNum()
+    if (num) {
+      return arr[num - 1]
+    } else {
+      return false
+    }
+  }
+}
+
+// 工具函数区======================================
+// 弹窗
+var notice = function(text) {
+  var $notice = $('#notice')
+
+  if (!$notice.length) {
+    $notice = $('<div id="notice" class="inout">' + text + '</div>')
+    $('body').append($notice)
+  } else {
+    $notice.html(text).removeClass('inout').show()
+    $notice.addClass('inout')
+  }
+}
+
+// 普通模式网络测试
+function nosu_test() {
+  net.test('https://youtube.com').then(function() {
+    monkey.open()
+  }).catch(function(err) {
+    monkey.close()
+    throw new Error(err)
+  })
+}
+
+// proxy url 替换
+var setProxy = function(prefix) {
+  prefix = prefix || ''
+  var p = {}
+  p.google = prefix + $('#google').data('to')
+  p.pic = prefix + $('#picsearch').data('to')
+  p.pic_h = prefix + $('#picsearch').data('home')
+  p.youtube = prefix + $('#youtube').data('to')
+  p.youtube_h = prefix + $('#youtube').data('home')
+
+  if (!prefix) {
+    $.each(p, function(key, val) {
+      p[key] = val.replace(/https:.+?https/, 'https')
+    })
+  }
+
+  if ($('#logo-google').hasClass('active')) {
+    $('#search_input').attr('data-to', p.google)
+  }
+
+  $('#google').attr('data-to', p.google)
+  $('#picsearch').attr('data-to', p.pic)
+  $('#picsearch').attr('data-home', p.pic_h)
+  $('#youtube').attr('data-to', p.youtube)
+  $('#youtube').attr('data-home', p.youtube_h)
+}
+
+// 检测网络，设置 proxy mode
+var setMode = function() {
+  net.test("https://youtube.com").then(function() {
+    console.log("Use direct mode.")
+    setProxy()
+    monkey.open()
+  }).catch(function(err) {
+    console.log("Use proxy mode.")
+    var promise_arr = []
+    var lock_val = lockNet.getVal()
+
+    if (lock_val) {
+      promise_arr = [net.test(lock_val)]
+    } else {
+      promise_arr = liveProxy(proxy_url)
+    }
+
+    oneSuccess(promise_arr).then(function(prefix) {
+      setProxy(prefix)
+      monkey.open()
+    }).catch(function(e) {
+      monkey.close()
+      throw new Error(e)
+    })
+  })
+}
+
+// url_arr 生成 promise_arr
+var liveProxy = function(url_arr) {
+  var promise_arr = []
+
+  $.each(url_arr, function(index, url) {
+    var pro = net.test(url)
+
+    promise_arr.push(pro)
+  })
+
+  return promise_arr
+}
+
+var openSU = function() {
+  monkey.color()
+  localStorage.setItem('SU', 'true')
+  setMode()
+}
+
+var offSU = function() {
+  monkey.gray()
+  localStorage.setItem('SU', 'false')
+  nosu_test()
+}
+
+// 第一个成功 promise
+function oneSuccess(promises) {
+  return Promise.all(promises.map(function(p) {
+    return p.then(function(val) {
+      return Promise.reject(val);
+    }, function(err) {
+      return Promise.resolve(err);
+    });
+  })).then(
+    function(errors) {
+      return Promise.reject(errors);
+    },
+    function(val) {
+      return Promise.resolve(val);
+    });
+}
+
+// 事件监听区====================================
+$(document).on('click', '#btn-mode', function() {
+  if ($('#monkey').hasClass('gray')) {
+    openSU()
+    notice('智能模式，低调使用!')
+  } else {
+    offSU()
+    notice('普通模式，自力更生!')
+  }
+})
+
+$(document).on('click', '#monkey.color', function() {
+  lockNet.next(function(index, val) {
+    if (index) {
+      notice('锁定线路：' + index)
+    } else {
+      notice('自动选线')
+    }
+
+    setMode()
+  })
+})
+
+var handleVisibilityChange = function() {
+  if (document.hidden) {
+    console.log('Background')
+  } else {
+    console.log('Front')
+    if ($('#monkey').hasClass('gray')) {
+      nosu_test()
+    } else {
+      setMode()
+    }
+  }
+}
+
+// PWA==========================================================================
+// sw register
+window.addEventListener("load", function() {
+  console.log("Will the service worker register?");
+  navigator.serviceWorker.register('/sw.js')
+  .then(function(reg){
+      console.log("Yes, it did.");
+  }).catch(function(err) {
+      console.log("No it didn't. This happened: ", err)
+  });
+});
---
+++ sw.js
@@ -0,0 +1,117 @@
+// configuration
+const
+  version = '1.0.1',
+  CACHE = version + '::PWAsite',
+  installFilesEssential = [
+    '/',
+    '/js/main.js',
+    '/sw.js',
+    'https://cdn.jsdelivr.net/npm/zepto@1.2.0/dist/zepto.min.js',
+    'https://cdn.jsdelivr.net/gh/alfg/ping.js@0.2.2/dist/ping.min.js'
+  ],
+  installFilesDesirable = [
+    '/favicon.ico',
+    '/img/so-zhihu.png',
+    '/img/so-youtube.png',
+    '/img/so-weibo.png',
+    '/img/so-translate.png',
+    '/img/so-pic.png',
+    '/img/so-taobao.png',
+    '/img/so-handian.png',
+    '/img/so-google.png',
+    '/img/so-douban.png',
+    '/img/so-dogedoge.png',
+    '/img/so-bing.png',
+    '/img/so-baidu.png',
+    '/img/logo-google.png',
+    '/img/logo-dogedoge.png',
+    '/img/logo-bing.png',
+    '/img/logo-baidu.png',
+  ];
+
+// install static assets
+function installStaticFiles() {
+  return caches.open(CACHE)
+    .then(cache => {
+      // cache desirable files
+      cache.addAll(installFilesDesirable);
+
+      // cache essential files
+      return cache.addAll(installFilesEssential);
+    });
+}
+
+// clear old caches
+function clearOldCaches() {
+  return caches.keys()
+    .then(keylist => {
+
+      return Promise.all(
+        keylist
+        .filter(key => key !== CACHE)
+        .map(key => caches.delete(key))
+      );
+    });
+}
+
+// application installation
+self.addEventListener('install', event => {
+  console.log('service worker: install');
+
+  // cache core files
+  event.waitUntil(
+    installStaticFiles()
+    .then(() => self.skipWaiting())
+  );
+});
+
+// application activated
+self.addEventListener('activate', event => {
+  console.log('service worker: activate');
+
+  // delete old caches
+  event.waitUntil(
+    clearOldCaches()
+    .then(() => self.clients.claim())
+  );
+});
+
+// application fetch network data
+self.addEventListener('fetch', event => {
+
+  // abandon non-GET requests
+  if (event.request.method !== 'GET') return;
+
+  let url = event.request.url;
+
+  event.respondWith(
+
+    caches.open(CACHE)
+    .then(cache => {
+
+      return cache.match(event.request)
+        .then(response => {
+
+          if (response) {
+            // return cached file
+            console.log('cache fetch: ' + url);
+            return response;
+          }
+
+          // make network request
+          return fetch(event.request)
+            .then(newreq => {
+
+              console.log('network fetch: ' + url);
+              if (newreq.ok && !/youtube\.com/.test(url)) {
+                console.log('cache: ' + url)
+                cache.put(event.request, newreq.clone());
+              }
+              return newreq;
+            })
+            // app is offline
+            .catch(() => {throw new Error('network error: ' + url)});
+        });
+    })
+  );
+});
